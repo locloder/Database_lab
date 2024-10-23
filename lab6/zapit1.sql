ea1a3f99dd969d73e0b016980f64362853726623
Artem Makarenko <locloder@gmail.com>
2024-10-23 09:15:25 +0300

    Delete lab6 directory

--- lab6/zapit1.sql
+++
@@ -1,4 +0,0 @@
-SELECT c.name AS city_name, r.name AS region_name
-FROM cities c
-JOIN regions r ON c.region = r.uuid
-WHERE c.population > 350000;
--- lab6/zapit2.sql
+++
@@ -1,4 +0,0 @@
-SELECT c.name AS city_name
-FROM cities c
-JOIN regions r ON c.region = r.uuid
-WHERE r.name = 'Nord';
